08682b0a8b013d78d2e31293f8d15907afe3f10e
tomtim90 <tommytimberlake@gmail.com>
2016-02-19 09:55:38 -0500

    Added files via upload

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,4 +1,4 @@
-walk(document.body);
+﻿walk(document.body);
 
 function walk(node) 
 {
@@ -31,13 +31,23 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
+	v = v.replace(/\bthe current year\b/g, "2016");
+	v = v.replace(/\bthe Current Year\b/g, "2016");
+	v = v.replace(/\bthe Current year\b/g, "2016");
+	v = v.replace(/\bthe current Year\b/g, "2016");
+	v = v.replace(/\bTHE CURRENT YEAR\b/g, "2016");
+	v = v.replace(/\bthe CURRENT YEAR\b/g, "2016");
+	v = v.replace(/\bThe CURRENT YEAR\b/g, "2016");
+	v = v.replace(/\bThe current year\b/g, "2016");
+	v = v.replace(/\bThe Current Year\b/g, "2016");
+	v = v.replace(/\bThe Current year\b/g, "2016");
+	v = v.replace(/\bThe current Year\b/g, "2016");
 	v = v.replace(/\bcurrent year\b/g, "2016");
 	v = v.replace(/\bCurrent Year\b/g, "2016");
 	v = v.replace(/\bCurrent year\b/g, "2016");
 	v = v.replace(/\bcurrent Year\b/g, "2016");
 	v = v.replace(/\bCURRENT YEAR\b/g, "2016");
+	v = v.replace(/\bC U R R E N T Y E A R\b/g, "2 0 1 6");
 	
 	textNode.nodeValue = v;
 }
-
-
