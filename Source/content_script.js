ef542effbba0d49954a0afdfe87613046777ea58
tomtim90 <tommytimberlake@gmail.com>
2016-02-19 09:34:25 -0500

    changed cloud-to-butt to current-year
    changing the nature of this script

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,11 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bcurrent year\b/g, "2016");
+	v = v.replace(/\bCurrent Year\b/g, "2016");
+	v = v.replace(/\bCurrent year\b/g, "2016");
+	v = v.replace(/\bcurrent Year\b/g, "2016");
+	v = v.replace(/\bCURRENT YEAR\b/g, "2016");
 	
 	textNode.nodeValue = v;
 }
